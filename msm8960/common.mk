be356067027095294a45826712ce7921a3b12169
Jimmy <jimmyjxiao@hotmail.com>
2016-03-06 23:03:44 +0000

    stuff
    Change-Id: I00a218e813d47c46f797f30e3c7cdfd757a7e204

--- msm8960/common.mk
+++ msm8960/common.mk
@@ -34,3 +34,6 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 endif
+
+common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
